bfd93986a2ba0ecd0e3685025c6fe9aa84236f55
eriveltonluiz <erivelton-paulista@hotmail.com>
2021-04-22 16:39:16 -0300

    Subindo api de crud da pessoa

---
+++ src/main/java/com/project/personapi/ApiCrudPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.project.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudPessoaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/personapi/controller/PersonController.java
@@ -0,0 +1,5 @@
+package com.project.personapi.controller;
+
+public class PersonController {
+
+}
