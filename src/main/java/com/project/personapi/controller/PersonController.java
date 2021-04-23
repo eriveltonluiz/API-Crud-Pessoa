36fb6c5c24abd89cafdc4458eb0d4bcabfa26bf8
eriveltonluiz <erivelton-paulista@hotmail.com>
2021-04-23 16:59:21 -0300

    Implementando operações de busca de pessoa por id e exclusão do mesmo

--- src/main/java/com/project/personapi/controller/PersonController.java
+++ src/main/java/com/project/personapi/controller/PersonController.java
@@ -7,13 +7,16 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.personapi.dto.request.PersonDTO;
+import com.project.personapi.exception.PersonNotFoundException;
 import com.project.personapi.model.Person;
 import com.project.personapi.service.PersonService;
 
@@ -28,14 +31,24 @@ public class PersonController {
 		this.personService = personService;
 	}
 	
-	
 	@GetMapping
-	public ResponseEntity<List<Person>> list(){
+	public ResponseEntity<List<PersonDTO>> listAll(){
 		return ResponseEntity.ok(personService.listOfPerson());
 	}
 
 	@PostMapping
 	public ResponseEntity<Person> createPerson(@RequestBody @Valid PersonDTO personDTO){
 		return ResponseEntity.status(HttpStatus.CREATED).body(personService.savePerson(personDTO));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PersonDTO> findById(@PathVariable Long id) throws PersonNotFoundException{
+		return ResponseEntity.ok(personService.findById(id));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) throws PersonNotFoundException{
+		personService.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/project/personapi/service/PersonService.java
+++ src/main/java/com/project/personapi/service/PersonService.java
@@ -1,11 +1,13 @@
 package com.project.personapi.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.personapi.dto.request.PersonDTO;
+import com.project.personapi.exception.PersonNotFoundException;
 import com.project.personapi.mapper.PersonMapper;
 import com.project.personapi.model.Person;
 import com.project.personapi.repository.PersonRepository;
@@ -28,7 +30,26 @@ public class PersonService {
 		return savedPerson;
 	}
 
-	public List<Person> listOfPerson() {
-		return personRepository.findAll();
+	public List<PersonDTO> listOfPerson() {
+		List<PersonDTO> people = personRepository.findAll()
+				.stream()
+				.map(person -> personMapper.toDTO(person))
+				.collect(Collectors.toList());
+		return people;
+	}
+
+	public PersonDTO findById(Long id) throws PersonNotFoundException {
+		Person person = verifyIfExistsId(id);
+		
+		return personMapper.toDTO(person);
+	}
+
+	public void deleteById(Long id) throws PersonNotFoundException {
+		verifyIfExistsId(id);
+		personRepository.deleteById(id);
+	}
+
+	private Person verifyIfExistsId(Long id) throws PersonNotFoundException {
+		return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
 	}
 }
